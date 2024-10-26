e6aef5b14fdda65d257a52956d9e45246c80932d
Rotem Dan <rotemdan@gmail.com>
2024-10-26 11:02:10 +0300

    Change Emscripten build to produce an ESM module, and try to provide web compatibility in `locateFile`.

--- emscripten/pre.js
+++ emscripten/pre.js
@@ -1,10 +1,23 @@
 Module['locateFile'] = function (path, prefix) {
-	//console.log("locateFile:", path, prefix)
-	//console.log("__dirname:", __dirname)
+	//console.log(`locateFile called, with path: ${path}, prefix: ${prefix}`)
 
-	if (path == "espeak-ng.data") {
-		return __dirname + "/" + "espeak-ng.data"
+	if (path === "espeak-ng.data") {
+		const currentModuleUrl = import.meta.url
+
+		const lastSlashIndex = currentModuleUrl.lastIndexOf('/')
+
+		let directoryPath
+
+		if (currentModuleUrl.startsWith('file:///')) {
+			directoryPath = currentModuleUrl.substring(8, lastSlashIndex)
+		} else {
+			directoryPath = currentModuleUrl.substring(0, lastSlashIndex)
+		}
+
+		//console.log(`directoryPath: ${directoryPath}\n\n`)
+
+		return `${directoryPath}/espeak-ng.data`
 	} else {
-		return prefix + path
+		return `${prefix}${path}`
 	}
 }
